c5be8ef9fd8400c2597bdc5f2d397d4b9271d3a5
kasankal <106431952+Kasankal@users.noreply.github.com>
2022-07-13 19:34:55 +0300

    fixed a major but
    no more errors when starting the server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
+
 export default function Home() {
     return (
-      <body className="max-w-7xl mx-auto text-neutral-90  bg-neutral-400 dark:bg-white">
         <nav className="mx-auto p-4 bg-amber-400">
           <div className="container mx-auto flex items-center justify-between">
             <a href="/" 
@@ -32,12 +32,18 @@ export default function Home() {
             ring-offset-amber-400
             text-neutral-900
             hover:text-neutral-600
-            transition-colors
+            transition-color
             
-            ">
+            "aria-expanded="false"
+            aria-label="Open Menu">
+              {
+                
+              }
               <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
+
+
             </button>
             <div role="menubar" className="
             flex
@@ -119,7 +125,6 @@ export default function Home() {
             </div>
           </div>
         </nav>
-      </body>
     )
   }
 
