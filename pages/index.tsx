84ce933556d92b68890e9ae2aeeba72ac39d14ee
kasankal <106431952+Kasankal@users.noreply.github.com>
2022-07-14 17:31:05 +0300

    fixed more bugs
    come on deploy already

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,7 +71,7 @@ export default function Home() {
                 
               }
               <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                <path strokeLinecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
 
 
